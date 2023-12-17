ccdd2e03aa579d7cc17975653e7e16a64b4cf87e
nohheeyeon <heeyeon1213@gmail.com>
2023-12-17 18:17:50 +0900

    🐎Feat : "스프링 부트 연동 테스트를 위한 임시 API 호출 코드 추가"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {    
+  const [message, setMessage] = useState("");
 
-  const hello = "안녕!"
-  const element = <h1>{hello}</h1>
-
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.text())
+      .then(message => {
+        setMessage(message);
+      });
+  },[])
   return (
-    <div>
-      hello {hello}
-      {element}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">{message}</h1>
+        </header>
+        <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
     </div>
-  );
+  )
 }
 
 export default App;
