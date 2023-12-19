04ac29f5c7eed35fe11dcba4521ef597610e5b94
nohheeyeon <heeyeon1213@gmail.com>
2023-12-20 03:08:51 +0900

    🎨Refactor : "App 컴포넌트에서 예제를 CartPage로 교체"

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, {useState, useEffect} from "react";
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CartPage from './CartPage'; // CartPage 컴포넌트 임포트
 
-function App () {    
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => response.text())
-      .then(message => {
-        setMessage(message);
-      });
-  },[])
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">{message}</h1>
-        </header>
-        <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <CartPage /> {/* CartPage 컴포넌트 사용 */}
     </div>
-  )
+  );
 }
 
 export default App;
